6320e5365c58d0aeb4be4b7d90603d418525dd5b
Shravani2621 <167876745+Shravani2621@users.noreply.github.com>
2024-05-30 21:23:12 +0530

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
 import pandas as pd
 import time
 from bs4 import BeautifulSoup
