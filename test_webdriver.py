6839eb141fbac0aaf95daa3a2f05084bcf03ca77
Shravani2621 <167876745+Shravani2621@users.noreply.github.com>
2024-05-30 12:48:26 +0530

    Update test_webdriver.py

--- test_webdriver.py
+++ test_webdriver.py
@@ -3,14 +3,16 @@ from selenium import webdriver
 # Path to the Microsoft Edge WebDriver executable
 edge_driver_path = 'C:\\Users\\sss927832\\Downloads\\edgedriver_win64\\msedgedriver.exe'
 
+edge_options.add_argument('headless')
+
 # Initialize the Edge WebDriver
 edge_options = webdriver.EdgeOptions()
 # Add any additional options if needed
 # e.g., to run Edge in headless mode: edge_options.add_argument('headless')
-#driver = webdriver.Edge(executable_path=edge_driver_path, options=edge_options)
+driver = webdriver.Edge(executable_path=edge_driver_path, options=edge_options)
 
 # Navigate to a webpage
-#driver.get('https://www.youtube.com')
+driver.get('https://www.youtube.com')
 
 # Print the title of the webpage
 print("Title of the webpage:", driver.title)
