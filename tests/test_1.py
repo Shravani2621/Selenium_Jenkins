6991e3508b8542a91d2d71343fb4137b86f7845e
Shravani2621 <167876745+Shravani2621@users.noreply.github.com>
2024-05-30 21:09:56 +0530

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,60 +1,27 @@
-# Creating an Item
-
-
-import time
-
 from selenium import webdriver
-import pytest
+from selenium.webdriver.remote import webelement
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from pynput.keyboard import Key, Controller
-import openpyxl
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
+from bs4 import BeautifulSoup
+import os
+from datetime import datetime
+from selenium.webdriver import ActionChains
 
+driver = webdriver.Edge(executable_path = 'C:\\Users\\sss927832\\Downloads\\edgedriver\\msedgedriver.exe')
+def get_trulia_estimate(address):
+    driver.get('https://www.trulia.com/')
+    print(address)
+    element = (By.ID, 'homepageSearchBoxTextInput')
 
-def wait_for_window(self, timeout=2):
-    time.sleep(round(timeout / 1000))
-    wh_now = self.driver.window_handles
-    wh_then = self.vars["window_handles"]
-    if len(wh_now) > len(wh_then):
-        return set(wh_now).difference(set(wh_then)).pop()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(element)).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(element)).send_keys(address)
 
+    search_button = (By.CSS_SELECTOR, "button[data-auto-test-id='searchButton']")
 
-@pytest.mark.login
-def test():
-    driver = webdriver.Edge()
-    domain = "http://inbr2wks-249497:3000/"
-    driver.get(domain)
-    time.sleep(2)
-    path = r"C:\Users\sss927832\OneDrive - Tata Technologies\Desktop\Book1.xlsx"
-    b = openpyxl.load_workbook(path)
-    sheet = b.active
-    c1 = sheet.cell(row=5, column=1)
-    # driver.find_element(By.NAME, "username").send_keys("infodba")
-    driver.find_element(By.NAME, "username").send_keys(c1.value)
-    # driver.find_element(By.NAME, "password").send_keys("infodba")
-    driver.find_element(By.NAME, "password").send_keys(c1.value)
-    time.sleep(5)
-    driver.find_element(By.CSS_SELECTOR, ".sw-button.accent-caution").click()
-    time.sleep(10)
-    driver.find_element(By.CSS_SELECTOR,
-                        ".sw-column.aw-tile-tileContainer.aw-theme-locationsTile.aw-tile-smallSize").click()
-    time.sleep(5)
-    # driver.find_element(By.CSS_SELECTOR, "aw-commands-commandIconButton.aw-commands-command.aw-commandId"
-    #                                      "-Awp0ShowCreateObject.aw-commands-commandWrapperHorizontal").click()
-    # driver.find_element(By.CSS_SELECTOR, "aw-commandIcon").click()
-    driver.find_element(By.CSS_SELECTOR, ".aw-commands-commandIconButton.aw-commands-command.aw-commandId"
-                                         "-Awp0ShowCreateObject.aw-commands-commandWrapperHorizontal").click()
-    time.sleep(5)
-    driver.find_element(By.XPATH, "//div[@class='sw-lov-container aw-widgets-droppable']/child::input").send_keys(
-        "Item")
-    # driver.find_element(By.CSS_SELECTOR, ".sw-property.sw-component.aw-widgets-droppable > "
-    # ".sw-aria-border").send_keys("Item")
-    # driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) > .sw-aria-border > .sw-row").
-    keyboard = Controller()
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
-    # driver.find_element(By.CSS_SELECTOR, ".sw-aria-border.aw-widgets-cellListItem.aw-widgets-cellTop").click()
-    time.sleep(5)
-    driver.find_element(By.NAME, "object_name").send_keys("Item2")
-    time.sleep(5)
-    driver.find_element(By.CSS_SELECTOR, ".aw-panel-footer.sw-row.flex-wrap > .sw-button").click()
-    time.sleep(5)
+    WebDriverWait(driver, 50).until(EC.element_to_be_clickable(search_button)).click()
+
+    time.sleep(3) 
