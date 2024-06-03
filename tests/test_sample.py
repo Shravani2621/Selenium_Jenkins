74fda37816e683af40555f1610a7aa9221b58df9
Shravani2621 <Shravani.S@tatatechnologies.com>
2024-06-03 15:53:35 +0530

    New code

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -8,7 +8,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from pynput.keyboard import Key, Controller
 import openpyxl
-from selenium.webdriver import Chrome
+
 
 def wait_for_window(self, timeout=2):
     time.sleep(round(timeout / 1000))
@@ -18,13 +18,13 @@ def wait_for_window(self, timeout=2):
         return set(wh_now).difference(set(wh_then)).pop()
 
 
-#@pytest.mark.login
+@pytest.mark.login
 def test():
-    driver = Chrome()
+    driver = webdriver.chrome()
     domain = "http://inbr2wks-249497:3000/"
     driver.get(domain)
-    time.sleep(10)
-    path = r"C:\Users\sss927832\Desktop\Book1.xlsx"
+    time.sleep(2)
+    path = r"C:\Users\sss927832\OneDrive - Tata Technologies\Desktop\Book1.xlsx"
     b = openpyxl.load_workbook(path)
     sheet = b.active
     c1 = sheet.cell(row=5, column=1)
@@ -32,18 +32,18 @@ def test():
     driver.find_element(By.NAME, "username").send_keys(c1.value)
     # driver.find_element(By.NAME, "password").send_keys("infodba")
     driver.find_element(By.NAME, "password").send_keys(c1.value)
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.CSS_SELECTOR, ".sw-button.accent-caution").click()
     time.sleep(10)
     driver.find_element(By.CSS_SELECTOR,
                         ".sw-column.aw-tile-tileContainer.aw-theme-locationsTile.aw-tile-smallSize").click()
-    time.sleep(10)
+    time.sleep(5)
     # driver.find_element(By.CSS_SELECTOR, "aw-commands-commandIconButton.aw-commands-command.aw-commandId"
     #                                      "-Awp0ShowCreateObject.aw-commands-commandWrapperHorizontal").click()
     # driver.find_element(By.CSS_SELECTOR, "aw-commandIcon").click()
     driver.find_element(By.CSS_SELECTOR, ".aw-commands-commandIconButton.aw-commands-command.aw-commandId"
                                          "-Awp0ShowCreateObject.aw-commands-commandWrapperHorizontal").click()
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.XPATH, "//div[@class='sw-lov-container aw-widgets-droppable']/child::input").send_keys(
         "Item")
     # driver.find_element(By.CSS_SELECTOR, ".sw-property.sw-component.aw-widgets-droppable > "
@@ -53,8 +53,8 @@ def test():
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
     # driver.find_element(By.CSS_SELECTOR, ".sw-aria-border.aw-widgets-cellListItem.aw-widgets-cellTop").click()
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.NAME, "object_name").send_keys("Item2")
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.CSS_SELECTOR, ".aw-panel-footer.sw-row.flex-wrap > .sw-button").click()
-    time.sleep(10)
+    time.sleep(5)
