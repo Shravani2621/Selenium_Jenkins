bb2a8d1a35e2020ff8580f00f53cb483dc8d1d4f
Shravani <Shravani.S@tatatechnologies.com>
2024-06-05 18:33:55 +0530

    Windchill Updates

--- tests_TeamCenter_AWC/test_UC_01.py
+++ tests_TeamCenter_AWC/test_UC_01.py
@@ -1,6 +1,4 @@
 # Creating an Item
-#Author Sanchita
-
 import time
 
 from selenium import webdriver
@@ -9,16 +7,12 @@ from selenium.webdriver.common.by import By
 from pynput.keyboard import Key, Controller
 import openpyxl
 from selenium.webdriver import Chrome
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 # @pytest.mark.login
 def test():
     driver = Chrome()
     domain = "http://inbr2wks-249497:3000/"
     driver.get(domain)
-    driver.set_window_size(1100, 600)
     time.sleep(2)
     path = r'C:\Users\sss927832\Selenium+TC\Selenium+TC\Book1.xlsx'
     b = openpyxl.load_workbook(path)
@@ -56,10 +50,6 @@ def test():
     if driver.find_element(By.XPATH, "//div[@class='sw-lov-container aw-widgets-droppable']/child::input") == "true":
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
-    wait = WebDriverWait(driver, 10)  # 10 seconds timeout
-    element = wait.until(EC.element_to_be_clickable((By.NAME, 'object_name')))
-    element.click()
-    time.sleep(3)
     driver.find_element(By.NAME, "object_name").send_keys(c2.value)
     time.sleep(5)
     driver.find_element(By.CSS_SELECTOR, ".aw-panel-footer.sw-row.flex-wrap > .sw-button").click()
