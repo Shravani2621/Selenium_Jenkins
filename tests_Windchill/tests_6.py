10401a3e4cacbf89c57fc6b0307e76b5da6f7723
Shravani <Shravani.S@tatatechnologies.com>
2024-06-05 13:15:59 +0530

    Windchill Updates

--- tests_Windchill/tests_6.py
+++ tests_Windchill/tests_6.py
@@ -13,7 +13,7 @@ def wait_for_window(self, timeout=2):
         return set(wh_now).difference(set(wh_then)).pop()
 
 
-@pytest.mark.login
+# @pytest.mark.login
 def test():
     driver = Chrome()
     username = "traininguser"
