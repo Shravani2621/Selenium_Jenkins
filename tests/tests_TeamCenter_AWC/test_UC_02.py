eea7fb3f4bf061fe07c27f13dccb3671024bad39
Shravani <Shravani.S@tatatechnologies.com>
2024-06-10 14:24:04 +0530

    Folder Update

--- tests/tests_TeamCenter_AWC/test_UC_02.py
+++ tests/tests_TeamCenter_AWC/test_UC_02.py
@@ -17,19 +17,17 @@ def wait_for_window(self, timeout=2):
         return set(wh_now).difference(set(wh_then)).pop()
 
 
-# @pytest.mark.login
+@pytest.mark.login
 def test():
-    driver = Chrome()
+    driver = webdriver.Edge()
     domain = "http://inbr2wks-249497:3000/"
     driver.get(domain)
-    driver.set_window_size(1100, 600)
     time.sleep(2)
 
-    path = r"C:\Users\sss927832\Selenium+TC\Selenium+TC\Book1.xlsx"
+    path = r"C:\Users\sss927879\Desktop\Book1.xlsx"
     b = openpyxl.load_workbook(path)
     sheet = b.active
-    c1 = sheet.cell(row=2, column=5)
-    c2 = sheet.cell(row=2, column=8)
+    c1 = sheet.cell(row=5, column=1)
     driver.find_element(By.NAME, "username").send_keys(c1.value)
     driver.find_element(By.NAME, "password").send_keys(c1.value)
     time.sleep(5)
@@ -39,8 +37,7 @@ def test():
     # time.sleep(5)
     driver.find_element(By.CSS_SELECTOR, ".sw-button.accent-caution").click()
     time.sleep(40)
-    driver.find_element(By.CSS_SELECTOR,
-                        ".sw-column.aw-tile-tileContainer.aw-theme-locationsTile.aw-tile-smallSize").click()
+    driver.find_element(By.CSS_SELECTOR, ".sw-column.aw-tile-tileContainer.aw-theme-locationsTile.aw-tile-smallSize").click()
     time.sleep(5)
     driver.find_element(By.CSS_SELECTOR, ".aw-commands-commandIconButton.aw-commands-command.aw-commandId"
                                          "-Awp0ShowCreateObject.aw-commands-commandWrapperHorizontal").click()
@@ -48,14 +45,14 @@ def test():
     # driver.find_element(By.XPATH, "//a[contains(@aria-label, 'Part')]")
     # driver.find_element(By.CSS_SELECTOR, "li:nth-child(3) > .sw-aria-border > .sw-row").click()
     driver.find_element(By.CSS_SELECTOR, ".aw-widgets-droppable > .sw-aria-border").click()
-    time.sleep(5)
+    time.sleep(15)
     driver.find_element(By.XPATH, "//div[@class='sw-lov-container aw-widgets-droppable']/child::input").send_keys(
         "Part")
     keyboard = Controller()
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
-    time.sleep(25)
-    driver.find_element(By.CSS_SELECTOR, ".sw-changed").send_keys(c2.value)
-    time.sleep(25)
+    time.sleep(15)
+    driver.find_element(By.NAME, "object_name").send_keys("TestPart")
+    time.sleep(15)
     driver.find_element(By.CSS_SELECTOR, ".aw-panel-footer.sw-row.flex-wrap > .sw-button").click()
-    time.sleep(25)
+    time.sleep(15)
