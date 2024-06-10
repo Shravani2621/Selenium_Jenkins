bd3b92d343a279ffa0f128839094d8de4d521d1b
Shravani <Shravani.S@tatatechnologies.com>
2024-06-10 11:04:00 +0530

    Folder Update

--- tests/tests_TeamCenter_AWC/test_UC_01.py
+++ tests/tests_TeamCenter_AWC/test_UC_01.py
@@ -8,17 +8,17 @@ from pynput.keyboard import Key, Controller
 import openpyxl
 from selenium.webdriver import Chrome
 
-# @pytest.mark.login
+
+#@pytest.mark.login
 def test():
-    driver = Chrome()
+    driver = webdriver.Edge()
     domain = "http://inbr2wks-249497:3000/"
     driver.get(domain)
     time.sleep(2)
-    path = r'C:\Users\sss927832\Selenium+TC\Selenium+TC\Book1.xlsx'
+    path = r"C:\Users\sss927879\Desktop\Book1.xlsx"
     b = openpyxl.load_workbook(path)
     sheet = b.active
-    c1 = sheet.cell(row=2, column=5)
-    c2 = sheet.cell(row=2, column=7)
+    c1 = sheet.cell(row=5, column=1)
     # driver.find_element(By.NAME, "username").send_keys("infodba")
     driver.find_element(By.NAME, "username").send_keys(c1.value)
     # driver.find_element(By.NAME, "password").send_keys("infodba")
@@ -50,7 +50,9 @@ def test():
     if driver.find_element(By.XPATH, "//div[@class='sw-lov-container aw-widgets-droppable']/child::input") == "true":
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
-    driver.find_element(By.CSS_SELECTOR, ".sw-changed").send_keys(c2.value)
+    driver.find_element(By.NAME, "object_name").send_keys("TestItem")
     time.sleep(5)
     driver.find_element(By.CSS_SELECTOR, ".aw-panel-footer.sw-row.flex-wrap > .sw-button").click()
     time.sleep(5)
+
+
